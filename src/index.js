16eb81eab4a69fe67784164de0f7a3df7a75b482
Piyush Sharma <piyush.sharma1078@gmail.com>
2021-06-11 19:07:26 +0100

    basics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom"; //Package that consists of features such as the render method
 
-import './index.css';
-import App from './App';
+import "./index.css"; //The import below specifies that css file should be considered
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root")); //The following code states render <App /> where a document has a id or "root"(In this case index.html)
